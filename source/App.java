581198af1962029298052869319247380e5754ec
ItBlockZ <nontapat.boss@gmail.com>
2023-02-21 18:26:02 +0700

    add class Circle

--- source/App.java
+++ source/App.java
@@ -1,5 +1,6 @@
 package source;
 
+
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -18,16 +19,37 @@ public class App {
 
     private void detailComponents(){
         f.add(new JPanel() {
-            int xPos = 20;
-            int yPos = 50;
-            int radius = 10;
+            Circle a = new Circle(20, 50, 10, 200, Color.WHITE);
 
             @Override
             public void paint(Graphics g) {
                 super.paint(g);
                 setBackground(Color.BLACK);
-                g.setColor(Color.YELLOW);
-                g.fillOval(xPos, yPos, radius * 2, radius * 2);
+                draw(a, g);
+                if (a.getY() < 500) gravity(a, true);
+                else gravity(a, false);
+            }
+
+            private void gravity(Circle c, boolean hasGravity) {
+                if (!hasGravity) return;
+                if (c.getY() < getHeight() - (c.getRadius() * 2) - 5) {
+                    sleep(1000/c.getSpeed());
+                    c.translate(0, 1);
+                    repaint();
+                }
+            }
+
+            private void draw(Circle c, Graphics g) {
+                g.setColor(c.getColor());
+                g.fillOval(c.getX(), c.getY(), c.getRadius() * 2, c.getRadius() * 2);
+            }
+
+            private void sleep(int ms) {
+                try {
+                    Thread.sleep(ms);
+                } catch (InterruptedException e) {
+                    System.out.println(e);
+                }
             }
         });
     }
