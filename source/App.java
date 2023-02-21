e07dc0dfaddb52f2fd9d6d770de12588519214c5
moominyippy <nongtong3553@gmail.com>
2023-02-21 14:17:05 +0700

    สร้างเฟรมแล้วจ้า

--- source/App.java
+++ source/App.java
@@ -1,5 +1,19 @@
 package source;
 
+import javax.swing.JFrame;
+
 public class App {
-    //ขันหมาก
+    private JFrame f;
+    public App(){
+        f = new JFrame("Game");
+        f.setSize(600, 700);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        detailComponents();
+        f.setVisible(true);
+    }
+
+    private void detailComponents(){
+        
+    }
 }
+
