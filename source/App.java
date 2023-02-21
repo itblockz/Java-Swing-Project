1919d8bcdad0a73e2cde2cc3afbbfd8e178c6a3e
Jinnawat.jid <Jinnawat.jid@outlook.com>
2023-02-21 14:18:42 +0700

    ยังไม่เสร็จเคิ้บ

--- source/App.java
+++ source/App.java
@@ -1,5 +1,22 @@
 package source;
 
-public class App {
-    //ขันหมาก
+public class Gravity {
+    const GRAVITY = 10;
+const TERMINAL_VELOCITY = 300;
+
+object Player 
+{
+    int vertical_speed = 0;
+    int vertical_position;  
+
+    function fall ()
+    {
+        this.vertical_speed = this.vertical_speed + GRAVITY;
+        if (this.vertical_speed > TERMINAL_VELOCITY)
+        {
+            this.vertical_speed = TERMINAL_VELOCITY;
+        }
+        this.vertical_position = this.vertical_position - this.vertical_speed;
+    }
+}
 }
